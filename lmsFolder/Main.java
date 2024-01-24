470f0782e861325378227f1ad0a936f0b7e06fdd
tabietritha <116423104+tabietritha@users.noreply.github.com>
2024-01-24 16:39:53 +0300

    Update Main.java

--- lmsFolder/Main.java
+++ lmsFolder/Main.java
@@ -3,8 +3,5 @@ package lmsPack;
 public class Main {
     public static void main(String[] args) {
         new welcomePage();
-        //new addBooks();
-        //new Librarian();
-        //new RetrieveData();
     }
 }
